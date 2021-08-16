0e447a4ed794adf7e66dfc1c8a733f2115546a17
Grace <funmibiajadi@gmail.com>
2021-08-16 20:40:08 +0100

    Customer class (Banking app)

--- MavenBank/src/com/maven/bank/Customer.java
+++ MavenBank/src/com/maven/bank/Customer.java
@@ -1,2 +1,78 @@
-package com.maven.bank;public class Customer {
+package com.maven.bank;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Customer {
+    private long bvn;
+    private String firstName;
+    private String surname;
+    private String email;
+    private String address;
+    private String PhoneNumber;
+    private String password;
+    private  List<Account>accounts = new ArrayList<>();
+
+
+    public long getBvn() {
+        return bvn;
+    }
+
+    public void setBvn(long bvn) {
+        this.bvn = bvn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Account> getAccounts() {
+        return  accounts;
+    }
+
 }
