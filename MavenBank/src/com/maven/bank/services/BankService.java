faf0f4792ff68188ff9bea5b3eb66e250670c113
Grace <funmibiajadi@gmail.com>
2021-08-19 17:25:56 +0100

    Bank service

--- MavenBank/src/com/maven/bank/services/BankService.java
+++ MavenBank/src/com/maven/bank/services/BankService.java
@@ -1,9 +1,22 @@
 package com.maven.bank.services;
 
+import java.time.Year;
+
 public class BankService {
     private static long currentBVN = 2;
     private static long currentAccountNumber = 1000110003;
+    private static Year openingYear = Year.now().minusYears(2);
+    private static long currentTransactionId = 0;
 
+
+    public static Year getOpeningYear() {
+        return openingYear;
+    }
+
+
+    public static void setOpeningYear(Year openingYear) {
+        BankService.openingYear = openingYear;
+    }
     public static long getCurrentBVN() {
         return currentBVN;
     }
@@ -19,12 +32,22 @@ public class BankService {
     private static void setCurrentAccountNumber(long currentAccountNumber) {
         BankService.currentAccountNumber = currentAccountNumber;
     }
+    private static void setCurrentTransactionId(long currentTransactionId) {
+        BankService.currentTransactionId= currentTransactionId;
+    }
 
-
+    public static long getCurrentTransactionId() {
+        return currentTransactionId;
+    }
 
     public static long generateBVN(){
         currentBVN++;
         return  currentBVN;
+    }
+
+    public static long generateTransactionId(){
+        currentTransactionId++;
+        return  currentTransactionId;
     }
 
     public static long generateAccountNumber(){
