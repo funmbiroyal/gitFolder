1ef7f373bd2dc750f947d096804b085790adbe98
Grace <funmibiajadi@gmail.com>
2021-08-23 17:16:52 +0100

    Student service impl test

--- SchoolSystem/test/services/StudentServiceImplTest.java
+++ SchoolSystem/test/services/StudentServiceImplTest.java
@@ -1,8 +1,10 @@
 package services;
 
 import datastore.StudentRepo;
-import entitities.School;
-import entitities.Student;
+import entities.School;
+import entities.Student;
+import exceptions.InvalidIdException;
+import exceptions.InvalidRegistrationException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,7 +18,7 @@ public class StudentServiceImplTest {
 
 
     @Before
-    public void setUp()  {
+    public void setUp() {
         paul = new Student();
         school = new School();
         schoolService = new StudentServiceImpl();
@@ -27,17 +29,55 @@ public class StudentServiceImplTest {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
     }
 
     @Test
-    public void registerNewStudent(){
+    public void registerNewStudent() {
         assertNotNull(paul);
         assertTrue(StudentRepo.getStudents().isEmpty());
-        Student studentPaul = schoolService.registerStudent(paul);
-        school.addStudent(studentPaul);
-        assertEquals(1,school.getCurrentNumberOfStudents());
-       // System.out.println(StudentRepo.getStudents().size());
+        schoolService.registerStudent(paul);
+        assertEquals(1, School.getStudents().size());
 
     }
+
+    @Test
+    public void loginRegisteredStudent() {
+        try {
+        assertNotNull(paul);
+        assertTrue(StudentRepo.getStudents().isEmpty());
+        schoolService.registerStudent(paul);
+        assertEquals(1, School.getStudents().size());
+        assertTrue(paul.attemptToLogIn("ajegunle", "pass"));
+        } catch (InvalidIdException | InvalidRegistrationException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Test
+    public void loginWithWithWrongId() {
+        assertNotNull(paul);
+        assertTrue(StudentRepo.getStudents().isEmpty());
+        schoolService.registerStudent(paul);
+        assertThrows(InvalidIdException.class,()->paul.attemptToLogIn("ajegunle", "pas"));
+
+    }
+
+    @Test
+    public void studentCanApplyForCourses() {
+        try {
+            assertNotNull(paul);
+            assertTrue(StudentRepo.getStudents().isEmpty());
+            schoolService.registerStudent(paul);
+            assertEquals(1, School.getStudents().size());
+            assertTrue(paul.attemptToLogIn("ajegunle", "pass"));
+            String[] courses = {"Java", "python", "javascript", "web"};
+            paul.applyForCourses( courses);
+        } catch (InvalidIdException | InvalidRegistrationException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
