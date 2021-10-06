00882c42dc1eed45122e1292f9231e5ad92f82c4
Grace <funmibiajadi@gmail.com>
2021-10-06 04:57:20 +0100

    Student repo

--- SchoolSystem/src/datastore/StudentRepo.java
+++ SchoolSystem/src/datastore/StudentRepo.java
@@ -1,2 +1,36 @@
-package datastore;public class StudentRepo {
+package datastore;
+
+import entities.School;
+import entities.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentRepo {
+
+
+    private static Map<String, Student> students = new HashMap();
+
+    public static Map<String, Student> getStudents() {
+        return students;
+    }
+
+    public static void setStudents(Map<String, Student> students) {
+        StudentRepo.students = students;
+    }
+  static {
+        setUp();
+       }
+
+    public static void setUp(){
+        Student studentPaul = new Student();
+        School school = new School();
+        studentPaul.setFirstName("paul");
+        studentPaul.setLastName("ajegunle");
+        studentPaul.setGender("male");
+        studentPaul.setDateOfBirth("10/12/2020");
+
+    }
+
+
 }
