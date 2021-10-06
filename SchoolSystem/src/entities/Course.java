40d876e0fb2a28f91b3882c3f1ee53e0e04e2d5f
Grace <funmibiajadi@gmail.com>
2021-10-06 04:55:04 +0100

    Course class

--- SchoolSystem/src/entities/Course.java
+++ SchoolSystem/src/entities/Course.java
@@ -1,4 +1,38 @@
-package entitities;
+package entities;
 
 public class Course {
+    private String name;
+    private String courseCode;
+    private int duration;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+
+
+    public Course(String name) {
+        this.name = name;
+    }
 }
