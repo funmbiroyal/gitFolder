de46d48475eb8a1c2c117c99c1d5cb8da1945b71
Grace <funmibiajadi@gmail.com>
2021-10-06 04:49:01 +0100

    Student service implementation

--- SchoolSystem/src/services/StudentServiceImpl.java
+++ SchoolSystem/src/services/StudentServiceImpl.java
@@ -1,19 +1,43 @@
 package services;
 
-import entitities.Student;
+import entities.School;
+import entities.Student;
+import exceptions.InvalidIdException;
 
 public class StudentServiceImpl implements StudentService {
-    private StudentService schoolService;
-
 
     @Override
     public Student registerStudent(Student theStudent) {
+        School.getStudents().add(theStudent);
 
-      return theStudent;
+        return theStudent;
     }
 
-//    private Student addStudent(Student theStudent){
-//        schoolService.
-//    }
+@Override
+    public boolean validateLogIn(String username, String password) throws InvalidIdException {
+        for (Student student: School.getStudents()){
+            if (student.getLastName().equalsIgnoreCase(username)){
+                if(student.getPassword().equals(password)){
+                    return true;
+                }
+                else{ throw new InvalidIdException("Invalid password");}
+            }
+        }
+        throw new InvalidIdException("No student found with that Id");
+    }
+
+@Override
+    public boolean findStudent(String id)  {
+        boolean hasFound = false;
+        for (Student student:School.getStudents()){
+            if (id.equals(School.getStudents().get(0).getId())) {
+                hasFound = true;
+                break;
+            }
+        }
+        return hasFound;
+    }
+
 }
+
 
